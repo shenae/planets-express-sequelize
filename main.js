dbe3f47979c95e8385762e2abb38f9e03074bf36
John Master <john.master@ga.co>
2018-11-27 08:23:12 -0500

    Add main.js skeleton

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-const { Example } = require('./models');
+const { Planet } = require('./models');
+
+async function createPlanets() {
+  console.log('I should create three planets');
+}
+
+async function printPlanets() {
+  console.log('I should query for all planets and print the resulting datavalues');
+}
+
+async function changeColor() {
+  console.log('I should change the color of a planet');
+}
+
+async function deletePlanet() {
+  console.log('I should delete a planet');
+}
+
+async function run() {
+  await createPlanets();
+  await printPlanets();
+  await changeColor();
+  await deletePlanet();
+  await printPlanets();
+}
+
+run();
